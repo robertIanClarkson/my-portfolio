673e6c1cde5f880997a305d8c3be87ba897fabd9
Robert Clarkson <robert.ian.clarkson@gmail.com>
2021-05-12 18:14:43 -0700

    Starting on Navbar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Robert Clarkson', css: 'index.css' });
+  res.render('index', { 
+    title: 'Robert Clarkson', 
+    css: 'index.css',
+    js: 'index.js'
+  });
 });
 
 module.exports = router;
